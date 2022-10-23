301e87ca89a2dd4e1fcad35d9abaad7afa195326
BM <bm1120836@yandex.ru>
2022-10-23 09:06:22 +0300

    Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности

---
+++ Task3.py
@@ -0,0 +1,18 @@
+# Задайте последовательность чисел. 
+# Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
+
+from random import randint
+
+def create_list(size, m, n):
+    return [randint(m, n) for i in range(size)]
+
+def get_value(list):
+    return [i for i in set(list)]
+
+size = 10
+m = 1
+n = 100
+
+origin = create_list(size, m, n)
+print(origin)
+print(get_value(origin))
